82d1befa35751092a058045e628071d8d6d95f62
Lwando Sodinga <125615819+LwandoS30@users.noreply.github.com>
2025-04-22 11:09:02 +0200

    Add files via upload

--- Task_Node/server.js
+++ Task_Node/server.js
@@ -1,62 +1,65 @@
 const http = require('node:http');
 
 const hostname = '127.0.0.1'; 
-const port = 3000;
+const port = 3002;
 
-let myCategory = {
+let myCategory = 
+{
 	movies:[
-		{name:"action", types:["Working man", "G20","Novocaine"]},
-		{name:"Horror", types:["The soul eater", "Infested","Cloune Motel"]},
+		{name:"Action", types:["Working man", "G20","Novocaine"]},
+		{name:"Horror", types:["The soul eater", "Infested","Clown Motel"]},
 		{name:"Romance", types:["Forever", "Friends with benefits","Float"]}
-],
+    ],
 	series:[
-		{name:"animation", types["Bleach", "Naruto","Doctor stone"]},
-		{name:"comedy", types:["The studio", "Poppas's house","The cosby show"]},
-		{name:"fantasy", types:["scissor seven", "wolf king","The sandman"]}
-],
+		{name:"Animation", types:["Bleach", "Naruto","Doctor stone"]},
+		{name:"Comedy", types:["The studio", "Poppas's house","The cosby show"]},
+		{name:"Fantasy", types:["Scissor seven", "Wolf king","The sandman"]}
+    ],
 	music:[
-		{name:"hip hop", types:["The allegory", "MMLP2","The forever story"]},
-		{name:"r&b", types:["Malibu", "The breakthrough","Chocolate Factory"]},
-		{name:"kasi rap", types:["The street government", "Impande","Heads and Tales"]}
-],
+		{name:"Hip Hop", types:["The allegory", "MMLP2","The forever story"]},
+		{name:"R&B", types:["Malibu", "The breakthrough","Chocolate Factory"]},
+		{name:"Kasi rap", types:["The street government", "Impande","Heads and Tales"]}
+    ],
 	sports:[
-		{name:"Rugby", types:["the springboks", "the all blacks","the wallabies"]},
+		{name:"Rugby", types:["The springboks", "The all blacks","The wallabies"]},
 		{name:"Soccer", types:["Brazil", "Bafana bafana","Spain"]},
 		{name:"Cricket", types:["The proteas", "The blues","The windies"]}
-]
+    ]
 };
 
 const server = http.createServer((req, res) => {
+    //console.log(req.method);
 
-    console.log(req.url);
     if(req.url === '/myCategory'){
+
         if(res.method === 'POST'){
             res.statusCode = 200;
             res.setHeader =('Content-Type', 'application/json');
             res.end(JSON.stringify({message: 'created'}));
         }
         else if(res.method === 'DELETE'){
-            students.splice(0,1);
+            myCategory.music[1].types[1];
             res.statusCode = 200;
             res.setHeader =('Content-Type', 'application/json');
             res.end(JSON.stringify({message: 'deleted'}));
         }
-        else if(res.method === 'PUT'){
-            students[0] = 'Lwando';
+        else if(res.method === 'PUT'){//UPDATE
+            myCategory.series[1].types[3] = 'Sonic Boom';
             res.statusCode = 200;
             res.setHeader =('Content-Type', 'application/json');
             res.end(JSON.stringify({message: 'updated', myCategory: myCategory}));
         }
         else  if(res.method === 'GET'){
-            res.statusCode = 404;
-            res.end();
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({myCategory: myCategory}));
         }
 
     }
     else 
     {
         res.statusCode = 404;
-        res.setHeader =('Content-Type', 'application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({myCategory: myCategory}));
     }
     
