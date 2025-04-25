f7aae763f0caaa1aae1578004c2420cda5b08fd8
Lwando Sodinga <125615819+LwandoS30@users.noreply.github.com>
2025-04-25 10:15:29 +0200

    Update server.js

--- Task_Node/server.js
+++ Task_Node/server.js
@@ -3,69 +3,89 @@ const http = require('node:http');
 const hostname = '127.0.0.1'; 
 const port = 3002;
 
-let myCategory = 
-{
-	movies:[
-		{name:"Action", types:["Working man", "G20","Novocaine"]},
-		{name:"Horror", types:["The soul eater", "Infested","Clown Motel"]},
-		{name:"Romance", types:["Forever", "Friends with benefits","Float"]}
-    ],
-	series:[
-		{name:"Animation", types:["Bleach", "Naruto","Doctor stone"]},
-		{name:"Comedy", types:["The studio", "Poppas's house","The cosby show"]},
-		{name:"Fantasy", types:["Scissor seven", "Wolf king","The sandman"]}
-    ],
-	music:[
-		{name:"Hip Hop", types:["The allegory", "MMLP2","The forever story"]},
-		{name:"R&B", types:["Malibu", "The breakthrough","Chocolate Factory"]},
-		{name:"Kasi rap", types:["The street government", "Impande","Heads and Tales"]}
-    ],
-	sports:[
-		{name:"Rugby", types:["The springboks", "The all blacks","The wallabies"]},
-		{name:"Soccer", types:["Brazil", "Bafana bafana","Spain"]},
-		{name:"Cricket", types:["The proteas", "The blues","The windies"]}
-    ]
+//2)
+let movies = ["Working man", "G20","Novocaine","The soul eater"];
+let series = ["Bleach", "Naruto","Doctor stone","The studio"];
+let music = ["The allegory", "MMLP2", "The breakthrough","Chocolate Factory"];
+let sports = ["Rugby","Soccor","Cricket","Volly ball"];
+  
+//3)
+
+//1)
+
+const handleCategoryEndPoint = (req, res) => {
+    //console.log(req.method);
+    if(req.method === 'POST'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({message: 'created'}));
+    }
+    else if(req.method === 'DELETE'){
+        series.splice(0,1);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({message: 'deleted', series: seri}));
+    }
+    else if(req.method === 'PUT'){//UPDATE
+        series[3] = 'Sonic Boom';
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({message: 'updated', series: series}));
+    }
+    else  if(req.method === 'GET'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({series: series}));
+    }
+
 };
 
 const server = http.createServer((req, res) => {
-    //console.log(req.method);
-
-    if(req.url === '/myCategory'){
-
-        if(res.method === 'POST'){
-            res.statusCode = 200;
-            res.setHeader =('Content-Type', 'application/json');
-            res.end(JSON.stringify({message: 'created'}));
-        }
-        else if(res.method === 'DELETE'){
-            myCategory.music[1].types[1];
-            res.statusCode = 200;
-            res.setHeader =('Content-Type', 'application/json');
-            res.end(JSON.stringify({message: 'deleted'}));
-        }
-        else if(res.method === 'PUT'){//UPDATE
-            myCategory.series[1].types[3] = 'Sonic Boom';
-            res.statusCode = 200;
-            res.setHeader =('Content-Type', 'application/json');
-            res.end(JSON.stringify({message: 'updated', myCategory: myCategory}));
-        }
-        else  if(res.method === 'GET'){
+    if(req.url === '/movies'){ 
+        if(req.method === 'POST'){
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({myCategory: myCategory}));
+            res.end(JSON.stringify({message: 'created'}));
+        }  
+        else if(req.method === 'DELETE'){
+            movies.splice(0, 1);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({message: 'Deleted'}));
+        }
+        else if(req.method === 'PUT'){
+            movies[0] = "King Aurther"
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({message: 'created'}));
+        } 
+        else if(req.method === 'GET'){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({movies: movies}));
         }
 
+
     }
-    else 
-    {
-        res.statusCode = 404;
+    else if(req.url === '/series'){
+        res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({myCategory: myCategory}));
+        res.end(JSON.stringify({series: series}));
+
     }
-    
+    else if(req.url === '/music'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'appliaction/json');
+        res.end(JSON.stringify({music: music}));
+
+    }
+    else{
+        res.statusCode = 404;
+        res.end();
+    }
 
 });
 
-server.listen(port,hostname, ()=>{
-    console.log('server running');
+server.listen(port, hostname, ()=>{
+    console.log('server running smoothly');
 });
